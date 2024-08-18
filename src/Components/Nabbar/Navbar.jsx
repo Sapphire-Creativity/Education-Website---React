e9802301d95ac82dd1f45b91c809c072083e5d7f
Sapphire-Creativity <sapphirecreativity@gmail.com>
2024-08-18 19:12:56 +0100

    Fourth commit

--- src/Components/Nabbar/Navbar.jsx
+++ src/Components/Nabbar/Navbar.jsx
@@ -1,35 +1,51 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { MdCastForEducation } from "react-icons/md";
-import { links } from "../../data";
 import { HiMenu } from "react-icons/hi";
 import { IoClose } from "react-icons/io5";
+import { links } from "../../data";
 import "./Navbar.css";
 
 const Navbar = () => {
 	const [isNavShowing, setIsNavShowing] = useState(false);
+	const [navBackground, setNavBackground] = useState(false);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			if (window.scrollY > 150) {
+				setNavBackground(true);
+			} else {
+				setNavBackground(false);
+			}
+		};
+
+		window.addEventListener("scroll", handleScroll);
+
+		// Cleanup the event listener on component unmount
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
 
 	return (
-		<nav>
+		<nav className={navBackground ? "nav-scrolled" : ""}>
 			<div className="container nav-container">
 				<Link to="./" className="logo" onClick={() => setIsNavShowing(false)}>
 					<MdCastForEducation />
-					<h3>ACADEMIA </h3>
+					<h3>ACADEMIA</h3>
 				</Link>
 
-				<ul className={`nav-links ${isNavShowing ? "show-nav" : "hide-nav"} `}>
-					{links.map(({ name, path }, index) => {
-						return (
-							<li key={index}>
-								<NavLink
-									to={path}
-									className={({ isActive }) => (isActive ? "active-nav" : "")}
-									onClick={() => setIsNavShowing((prev) => !prev)}>
-									{name}
-								</NavLink>
-							</li>
-						);
-					})}
+				<ul className={`nav-links ${isNavShowing ? "show-nav" : "hide-nav"}`}>
+					{links.map(({ name, path }, index) => (
+						<li key={index}>
+							<NavLink
+								to={path}
+								className={({ isActive }) => (isActive ? "active-nav" : "")}
+								onClick={() => setIsNavShowing((prev) => !prev)}>
+								{name}
+							</NavLink>
+						</li>
+					))}
 				</ul>
 
 				<button
--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -42,20 +42,6 @@ const Contact = () => {
 
 			<section className="container">
 				<div className="contact-container">
-					<div
-						className="map"
-						style={{ width: "100%", height: "450px", border: 0 }}>
-						<iframe
-							src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d46830151.11795828!2d-119.8093025!3d44.24236485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited%20States!5e0!3m2!1sen!2sng!4v1722768232326!5m2!1sen!2sng"
-							width="600"
-							height="450"
-							style={{ border: "0" }}
-							allowFullScreen
-							loading="lazy"
-							referrerPolicy="no-referrer-when-downgrade"
-							title="Map of the United States"
-						/>
-					</div>
 					<div className="contact-form">
 						<p>
 							We'd love to hear from you! Whether you have questions, feedback,
